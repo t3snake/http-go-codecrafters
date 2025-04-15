1d93249b603730c253d41be7e415d97b77f817db
t3snake <t3snake@gmail.com>
2025-04-15 23:34:54 +0530

    Send an initial response if server is pinged

--- app/main.go
+++ app/main.go
@@ -6,10 +6,6 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
@@ -19,9 +15,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
+
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
