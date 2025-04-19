5d90a188388e1a4ba6c8157c52be85d1a9de48a1
t3snake <t3snake@gmail.com>
2025-04-19 22:10:58 +0530

    Parse request headers, add a new end point to test and refactoring

--- app/main.go
+++ app/main.go
@@ -30,32 +30,77 @@ func main() {
 	defer conn.Close()
 
 	request_buffer := make([]byte, 30000)
-	content_length, err := conn.Read(request_buffer)
+	request_length, err := conn.Read(request_buffer)
 	if err != nil {
 		fmt.Println("Error reading request: ", err.Error())
 	}
 
-	request_parts := bytes.Split(request_buffer[:content_length], []byte("\r\n"))
-	request_line := request_parts[0]
+	request_line, request_body, request_headers := ParseRequest(request_buffer[:request_length])
 
-	request_line_parts := bytes.Split(request_line, []byte(" "))
-	// http_method := request_line_parts[0]
-	target := string(request_line_parts[1])
-	// http_version := request_line_parts[2]
+	fmt.Printf("Request Body: %s", request_body)
 
-	if target == "/" {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	} else if strings.HasPrefix(target, "/echo/") {
-		response_content := strings.ReplaceAll(target, "/echo/", "")
+	http_method, url, _ := ParseRequestLine(request_line)
 
-		content_type := fmt.Sprintf(content_type_formatter, content_type_plaintext)
-		content_length := fmt.Sprintf(content_length_formatter, len(response_content))
+	if http_method == "GET" {
+		if url == "/" {
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 
-		response_headers := fmt.Sprintf("%s%s", content_type, content_length)
+		} else if strings.HasPrefix(url, "/echo/") {
+			response_content := strings.ReplaceAll(url, "/echo/", "")
 
-		response := fmt.Sprintf("HTTP/1.1 200 OK\r\n%s\r\n%s", response_headers, response_content)
-		conn.Write([]byte(response))
-	} else {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+			response := GenerateResponse(response_content, content_type_plaintext, "HTTP/1.1 200 OK")
+			conn.Write([]byte(response))
+
+		} else if url == "/user-agent" {
+			response_content := request_headers["User-Agent"]
+
+			response := GenerateResponse(response_content, content_type_plaintext, "HTTP/1.1 200 OK")
+			conn.Write([]byte(response))
+		} else {
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+
+		}
 	}
+
+}
+
+// ParseRequest takes the request string and returns request line as string, request body as string and map of request headers respectively
+func ParseRequest(request []byte) (string, string, map[string]string) {
+	request_parts := bytes.Split(request, []byte("\r\n"))
+	request_line := string(request_parts[0])
+	request_body := string(request_parts[len(request_parts)-1])
+
+	request_headers := make(map[string]string)
+
+	// last header ends with \r\n and immediately \r\n for separation between header si ignore the second last split
+	for i := 1; i < len(request_parts)-2; i++ {
+		header := string(request_parts[i])
+		key_val := strings.Split(header, ": ")
+
+		request_headers[key_val[0]] = key_val[1]
+	}
+
+	return request_line, request_body, request_headers
+
+}
+
+// ParseRequestLine takes the request line and returns the http verb, URL, http version respectively
+func ParseRequestLine(request_line string) (string, string, string) {
+	request_line_parts := strings.Split(request_line, " ")
+	http_method := request_line_parts[0]
+	target := string(request_line_parts[1])
+	http_version := request_line_parts[2]
+
+	return http_method, target, http_version
+}
+
+// GenerateResponse takes the content, the content type and response to generate and return response
+func GenerateResponse(content, content_type, response_line string) string {
+	content_type_header := fmt.Sprintf(content_type_formatter, content_type)
+	content_length_header := fmt.Sprintf(content_length_formatter, len(content))
+
+	response_headers := fmt.Sprintf("%s%s", content_type_header, content_length_header)
+
+	response := fmt.Sprintf("%s\r\n%s\r\n%s", response_line, response_headers, content)
+	return response
 }
